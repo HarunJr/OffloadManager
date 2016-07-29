30d742216ed51e2793afe6ae30aef6d7c4ca19fb
HARUN <harunm28@gmmail.com>
2016-07-30 02:10:21 +0300

    VehicleFragment and AddVehicle Activty layouts

--- src/main/java/com/harun/offloadmanager/AddVehicle.java
+++ src/main/java/com/harun/offloadmanager/AddVehicle.java
@@ -1,20 +1,20 @@
 package com.harun.offloadmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class AddVehicle extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_vehicle);
 
-        Constants.toolbar = (Toolbar) findViewById(R.id.main_activity_toolbar);
+        Constants.toolbar = (Toolbar) findViewById(R.id.add_vehicle_activity_toolbar);
         setSupportActionBar(Constants.toolbar);
 
         assert getSupportActionBar() != null;
-        getSupportActionBar().setTitle(R.string.vehicles_title_label);
+        getSupportActionBar().setTitle(R.string.add_vehicle_title);
     }
 }
---
+++ src/main/java/com/harun/offloadmanager/Constants.java
@@ -0,0 +1,27 @@
+package com.harun.offloadmanager;
+
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+/**
+ * Created by HARUN on 4/3/2016.
+ */
+public class Constants {
+    public static Toolbar toolbar = null;
+    public static TextView textView = null;
+    public static final String VEHICLE_ID = "_id";
+    public static final String VEHICLE_REG = "vehicleReg";
+    public static final String DAILY_TOTAL_COLLECTION = "vehicleCollection";
+    public static final String DAILY_TOTAL_EXPENSE = "vehicleExpense";
+
+    /**
+     The caller references the constants using <tt>Consts.EMPTY_STRING</tt>,
+     and so on. Thus, the caller should be prevented from constructing objects of
+     this class, by declaring this private constructor.
+     */
+    protected Constants(){
+        //this prevents even the native class from
+        //calling this ctor as well :
+        throw new AssertionError();
+    }
+}
